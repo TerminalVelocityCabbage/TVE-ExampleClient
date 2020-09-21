647b42fcaf558b78702952f7d093e44a5c287a5e
Brandon Davis <neusfear@gmail.com>
2020-09-21 18:52:38 -0500

    Cleanup GameClient and add TerminalVelocityRenderer as a dependency

---
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/EventHandler.java
@@ -0,0 +1,96 @@
+package com.terminalvelocitycabbage.exampleclient;
+
+import com.github.simplenet.packet.Packet;
+import com.terminalvelocitycabbage.engine.client.PingClient;
+import com.terminalvelocitycabbage.engine.debug.Log;
+import com.terminalvelocitycabbage.engine.events.HandleEvent;
+import com.terminalvelocitycabbage.engine.events.client.ClientConnectionEvent;
+import com.terminalvelocitycabbage.engine.events.client.ClientStartEvent;
+import com.terminalvelocitycabbage.engine.server.PacketTypes;
+import org.fusesource.jansi.AnsiConsole;
+
+import java.util.Scanner;
+
+import static com.terminalvelocitycabbage.exampleclient.GameClient.PORT;
+import static com.terminalvelocitycabbage.exampleclient.GameClient.ADDRESS;
+
+public class EventHandler {
+
+	@HandleEvent(ClientStartEvent.PRE_INIT)
+	public void onPreInit(ClientStartEvent event) {
+		//Enable Console colors
+		AnsiConsole.systemInstall();
+	}
+
+	@HandleEvent(ClientStartEvent.START)
+	public void onStart(ClientStartEvent event) {
+		try {
+			var pingClient = new PingClient(ADDRESS, PORT);
+			if (pingClient.ping().getResult()) {
+				GameClient.getInstance().connect(ADDRESS, PORT);
+			} else {
+				Log.error("Server not found.");
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@HandleEvent({ClientConnectionEvent.CONNECT})
+	public void onConnect(ClientConnectionEvent event) {
+		var scanner = new Scanner(System.in);
+
+		// If messages arrive from other clients, print them to the console.
+		event.getClient().readByteAlways(opcode -> {
+			switch (opcode) {
+				case PacketTypes.CHAT:
+					event.getClient().readString(System.out::println);
+			}
+		});
+
+		System.out.println("Enter a username: ");
+		GameClient.getInstance().setId(scanner.nextLine());
+
+		//Send a username packet to the server with the id
+		Packet.builder().putByte(PacketTypes.CLIENT_VALIDATION).putString(GameClient.getInstance().getID()).queueAndFlush(event.getClient());
+
+		//Accept user-input for the chat server.
+		while (!GameClient.getInstance().disconnected()) {
+			var message = scanner.nextLine();
+			Packet.builder().putByte(PacketTypes.CHAT).putString(message).queueAndFlush(event.getClient());
+		}
+	}
+
+	@HandleEvent({ClientConnectionEvent.POST_DISCONNECT})
+	public void onDisconnect(ClientConnectionEvent event) {
+		if (!GameClient.getInstance().disconnected()) {
+			try {
+				Log.warn("Lost connection to the server.");
+				GameClient.getInstance().reconnect(ADDRESS, PORT, 10, 3);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@HandleEvent({ClientConnectionEvent.PRE_RECONNECT})
+	public void preReconnect(ClientConnectionEvent event) {
+		Log.info("Attempting to Reconnect...");
+	}
+
+	@HandleEvent({ClientConnectionEvent.RECONNECT_TRY_FAIL})
+	public void reconnectTryFail(ClientConnectionEvent event) {
+		Log.info("Attempt failed...");
+	}
+
+	@HandleEvent({ClientConnectionEvent.POST_RECONNECT})
+	public void postReconnect(ClientConnectionEvent event) {
+		Log.info("Reconnected.");
+	}
+
+	@HandleEvent({ClientConnectionEvent.RECONNECT_FAIL})
+	public void reconnectFail(ClientConnectionEvent event) {
+		Log.error("Reconnect failure.");
+	}
+
+}
--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameClient.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameClient.java
@@ -1,24 +1,17 @@
 package com.terminalvelocitycabbage.exampleclient;
 
-import com.github.simplenet.packet.Packet;
 import com.terminalvelocitycabbage.engine.client.ClientBase;
-import com.terminalvelocitycabbage.engine.client.PingClient;
-import com.terminalvelocitycabbage.engine.debug.Log;
-import com.terminalvelocitycabbage.engine.events.HandleEvent;
-import com.terminalvelocitycabbage.engine.events.client.ClientConnectionEvent;
-import com.terminalvelocitycabbage.engine.events.client.ClientStartEvent;
-import com.terminalvelocitycabbage.engine.server.PacketTypes;
-import org.fusesource.jansi.AnsiConsole;
-
-import java.util.Scanner;
 
 public class GameClient extends ClientBase {
+
+	public static GameClient instance;
 
 	public static final String ADDRESS = "localhost";
 	public static final int PORT = 49056;
 
 	public GameClient() {
-		addEventHandler(this);
+		instance = this;
+		addEventHandler(new EventHandler());
 		init();
 		start();
 	}
@@ -27,81 +20,7 @@ public class GameClient extends ClientBase {
 		new GameClient();
 	}
 
-	@HandleEvent(ClientStartEvent.PRE_INIT)
-	public void onPreInit(ClientStartEvent event) {
-		//Enable Console colors
-		AnsiConsole.systemInstall();
+	public static GameClient getInstance() {
+		return instance;
 	}
-
-	@HandleEvent(ClientStartEvent.START)
-	public void onStart(ClientStartEvent event) {
-		try {
-			var pingClient = new PingClient(ADDRESS, PORT);
-			if (pingClient.ping().getResult()) {
-				connect(ADDRESS, PORT);
-			} else {
-				Log.error("Server not found.");
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@HandleEvent({ClientConnectionEvent.CONNECT})
-	public void onConnect(ClientConnectionEvent event) {
-		var scanner = new Scanner(System.in);
-
-		// If messages arrive from other clients, print them to the console.
-		event.getClient().readByteAlways(opcode -> {
-			switch (opcode) {
-				case PacketTypes.CHAT:
-					event.getClient().readString(System.out::println);
-			}
-		});
-
-		System.out.println("Enter a username: ");
-		setId(scanner.nextLine());
-
-		//Send a username packet to the server with the id
-		Packet.builder().putByte(PacketTypes.CLIENT_VALIDATION).putString(getID()).queueAndFlush(event.getClient());
-
-		//Accept user-input for the chat server.
-		while (!disconnected()) {
-			var message = scanner.nextLine();
-			Packet.builder().putByte(PacketTypes.CHAT).putString(message).queueAndFlush(event.getClient());
-		}
-	}
-
-	@HandleEvent({ClientConnectionEvent.POST_DISCONNECT})
-	public void onDisconnect(ClientConnectionEvent event) {
-		if (!disconnected()) {
-			try {
-				Log.warn("Lost connection to the server.");
-				reconnect(ADDRESS, PORT, 10, 3);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	@HandleEvent({ClientConnectionEvent.PRE_RECONNECT})
-	public void preReconnect(ClientConnectionEvent event) {
-		Log.info("Attempting to Reconnect...");
-	}
-
-	@HandleEvent({ClientConnectionEvent.RECONNECT_TRY_FAIL})
-	public void reconnectTryFail(ClientConnectionEvent event) {
-		Log.info("Attempt failed...");
-	}
-
-	@HandleEvent({ClientConnectionEvent.POST_RECONNECT})
-	public void postReconnect(ClientConnectionEvent event) {
-		Log.info("Reconnected.");
-	}
-
-	@HandleEvent({ClientConnectionEvent.RECONNECT_FAIL})
-	public void reconnectFail(ClientConnectionEvent event) {
-		Log.error("Reconnect failure.");
-	}
-
 }
