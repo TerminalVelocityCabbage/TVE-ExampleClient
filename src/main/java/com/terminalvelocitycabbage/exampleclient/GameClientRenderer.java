3d765770344621a6e835ece6217ef524f884c05b
Brandon Davis <neusfear@gmail.com>
2020-09-24 18:40:06 -0500

    make a hard coded ebo for now

--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
@@ -3,8 +3,11 @@ package com.terminalvelocitycabbage.exampleclient;
 import com.terminalvelocitycabbage.engine.resources.Identifier;
 import com.terminalvelocitycabbage.engine.shader.ShaderHandler;
 import com.terminalvelocitycabbage.terminalvelocityrenderer.Renderer;
-import org.joml.Vector3f;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL;
+
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.ASSETS_ROOT_RESOURCE_MANAGER;
 import static org.lwjgl.glfw.GLFW.*;
@@ -33,6 +36,61 @@ public class GameClientRenderer extends Renderer {
 		//Cleanup shaders and bind the program
 		defaultShaderHandler.setupShaders();
 
+		float[] rectVertices = new float[] {
+				-0.8f, 	0.8f,	//0: top left
+				0.8f, 	0.8f,	//1: top right
+				-0.8f, 	-0.8f,	//2: btm left
+				0.8f, 	-0.8f,	//3: btm right
+		};
+		float[] colors = new float[] {
+				1, 0, 0, 1,
+				0, 1, 0, 1,
+				0, 0, 1, 1,
+				1, 1, 1, 1
+		};
+		short[] indices = new short[] {
+				0, 1, 2,	//left tri
+				1, 2, 3		//right tri
+		};
+
+		int vaoID = glGenVertexArrays();
+		glBindVertexArray(vaoID);
+
+		//Create vertices buffer I guess
+		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(rectVertices.length);
+		verticesBuffer.put(rectVertices).flip();
+
+		int vboVertID = glGenBuffers();
+		glBindBuffer(GL_ARRAY_BUFFER, vboVertID);
+		glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 2, GL_FLOAT, false, 0, 0);
+
+		//Create Color buffer I guess
+		FloatBuffer colorsBuffer = BufferUtils.createFloatBuffer(colors.length);
+		colorsBuffer.put(colors).flip();
+
+		int vboColID = glGenBuffers();
+		glBindBuffer(GL_ARRAY_BUFFER, vboColID);
+		glBufferData(GL_ARRAY_BUFFER, colorsBuffer, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(1, 4, GL_FLOAT, false, 0, 0);
+
+		ShortBuffer indicesBuffer = BufferUtils.createShortBuffer(indices.length);
+		indicesBuffer.put(indices).flip();
+
+		//Create element buffer I guess
+		int eboID = glGenBuffers();
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices, GL_STATIC_DRAW);
+
+		//Enable attribute locations
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+
+		glBindVertexArray(0);
+
+		/*
 		//Bind VAO
 		int vao = glGenVertexArrays();
 		glBindVertexArray(vao);
@@ -49,6 +107,7 @@ public class GameClientRenderer extends Renderer {
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(1, 3, GL_FLOAT, false, Float.BYTES * 6, Float.BYTES * 3);
 		glEnableVertexAttribArray(1);
+		 */
 
 		//For wireframe mode
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -64,10 +123,12 @@ public class GameClientRenderer extends Renderer {
 
 			//Actual Logic
 			//TODO add something real
+			glBindVertexArray(vaoID);
 
 			//Render the triangle
-			glBindVertexArray(vao);
-			glDrawArrays(GL_TRIANGLES, 0, 3);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+
+			glBindVertexArray(0);
 
 			//Send the frame
 			glfwSwapBuffers(getWindow());
@@ -75,8 +136,8 @@ public class GameClientRenderer extends Renderer {
 		}
 
 		//Clean up stuff (technically optional but good to know)
-		glDeleteVertexArrays(vao);
-		glDeleteBuffers(vbo);
+		glDeleteVertexArrays(0);
+		glDeleteBuffers(0);
 		defaultShaderHandler.delete();
 	}
 }
--- src/main/resources/assets/testclient/shaders/tri.frag
+++ src/main/resources/assets/testclient/shaders/tri.frag
@@ -1,11 +1,11 @@
 #version 330 core
-out vec4 FragColor;
 
-in vec3 ourColor;
+in vec4 ourColor;
+out vec4 fragColor;
 
 //uniform vec4 ourColor;
 
 void main()
 {
-   FragColor = vec4(ourColor, 1.0);
+   fragColor = ourColor;
 }
