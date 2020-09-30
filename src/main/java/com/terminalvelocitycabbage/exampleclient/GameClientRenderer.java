9a680699b9f288c094af5175cd4faa14a0d556d9
Brandon Davis <neusfear@gmail.com>
2020-09-30 17:24:12 -0500

    Improve the tests for positionMatrixes

--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
@@ -1,18 +1,19 @@
 package com.terminalvelocitycabbage.exampleclient;
 
 import com.terminalvelocitycabbage.engine.client.renderer.Camera;
-import com.terminalvelocitycabbage.engine.client.renderer.Renderer;
+import com.terminalvelocitycabbage.engine.client.renderer.RendererBase;
 import com.terminalvelocitycabbage.engine.client.resources.Identifier;
 import com.terminalvelocitycabbage.engine.client.shader.ShaderHandler;
+import com.terminalvelocitycabbage.engine.debug.Log;
 import com.terminalvelocitycabbage.exampleclient.shapes.TexturedRectangle;
 import com.terminalvelocitycabbage.exampleclient.shapes.TexturedVertex;
-import org.lwjgl.opengl.GL;
 
 import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.ASSETS_ROOT_RESOURCE_MANAGER;
+import static com.terminalvelocitycabbage.exampleclient.shapes.TexturedRectangle.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL20.*;
 
-public class GameClientRenderer extends Renderer {
+public class GameClientRenderer extends RendererBase {
 
 	public GameClientRenderer(int width, int height, String title) {
 		super(width, height, title);
@@ -20,17 +21,14 @@ public class GameClientRenderer extends Renderer {
 
 	@Override
 	public void loop() {
-		// creates the GLCapabilities instance and makes the OpenGL bindings available for use.
-		GL.createCapabilities();
-
 		Camera camera = new Camera((float)Math.toRadians(60.0f), 0.01f, 1000.0f, getWindow().width(), getWindow().height());
 
 		//Create a rectangle with a texture
 		TexturedRectangle rectangle = new TexturedRectangle(
-				new TexturedVertex().setXYZ(-0.5f, 0.5f, -2f).setRGB(255, 0, 0).setUv(0, 0),
-				new TexturedVertex().setXYZ(-0.5f, -0.5f, -2f).setRGB(0, 255, 0).setUv(0, 1),
-				new TexturedVertex().setXYZ(0.5f, -0.5f, -2f).setRGB(0, 0, 255).setUv(1, 1),
-				new TexturedVertex().setXYZ(0.5f, 0.5f, -2f).setRGB(255, 255, 255).setUv(1, 0),
+				new TexturedVertex().setXYZ(-0.5f, 0.5f, 0f).setRGB(255, 0, 0).setUv(0, 0),
+				new TexturedVertex().setXYZ(-0.5f, -0.5f, 0f).setRGB(0, 255, 0).setUv(0, 1),
+				new TexturedVertex().setXYZ(0.5f, -0.5f, 0f).setRGB(0, 0, 255).setUv(1, 1),
+				new TexturedVertex().setXYZ(0.5f, 0.5f, 0f).setRGB(255, 255, 255).setUv(1, 0),
 				new Identifier(GameClient.ID, "textures/kyle.png")
 		);
 		rectangle.bind();
@@ -57,34 +55,23 @@ public class GameClientRenderer extends Renderer {
 			//Init shader
 			defaultShaderHandler.use();
 
-			/*
-			//Randomize rectangle positions
-			// Define offset
-			float offsetX = (float) (Math.cos(Math.PI * glfwGetTime()) * 0.1);
-			float offsetY = (float) (Math.sin(Math.PI * glfwGetTime()) * 0.1);
+			//Make rectangle go further away every frame
 			//Add the offsets to each vertex
-			rectangle.getVertex(TOP_LEFT).addXYZW(offsetX, offsetY, 0f, 0f);
-			rectangle.getVertex(TOP_RIGHT).addXYZW(-offsetX, -offsetY, 0f, 0f);
-			rectangle.getVertex(BOTTOM_LEFT).addXYZW(offsetX, offsetY, 0f, 0f);
-			rectangle.getVertex(BOTTOM_RIGHT).addXYZW(-offsetX, -offsetY, 0f, 0f);
+			rectangle.getVertex(TOP_LEFT).addXYZW(0f, 0f, -0.005f, 0f);
+			rectangle.getVertex(TOP_RIGHT).addXYZW(0f, 0f, -0.005f, 0f);
+			rectangle.getVertex(BOTTOM_LEFT).addXYZW(0f, 0f, -0.005f, 0f);
+			rectangle.getVertex(BOTTOM_RIGHT).addXYZW(0f, 0f, -0.005f, 0f);
+
+			Log.info("" + rectangle.getVertex(0).getXYZW()[2]);
 
 			//Push the changes to openGL
 			rectangle.update();
-
-			//Reset the positions so it doesnt run away BUT dont push them to openGL, just the rectangle object
-			rectangle.getVertex(TOP_LEFT).addXYZW(-offsetX, -offsetY, 0f, 0f);
-			rectangle.getVertex(TOP_RIGHT).addXYZW(offsetX, offsetY, 0f, 0f);
-			rectangle.getVertex(BOTTOM_LEFT).addXYZW(-offsetX, -offsetY, 0f, 0f);
-			rectangle.getVertex(BOTTOM_RIGHT).addXYZW(offsetX, offsetY, 0f, 0f);
-
-			 */
 
 			//Draw whatever changes were pushed
 			rectangle.draw();
 
 			//Send the frame
-			glfwSwapBuffers(getWindowID());
-			glfwPollEvents();
+			push();
 		}
 
 		//Cleanup
