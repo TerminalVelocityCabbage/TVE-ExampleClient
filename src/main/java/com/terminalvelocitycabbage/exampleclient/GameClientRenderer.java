b92fc8df1147e7c0f5c0d532744c9a541ac899a5
Brandon Davis <neusfear@gmail.com>
2020-10-10 16:15:58 -0500

    Fix shaders and just use multiple programs instead of the other thing

--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
@@ -2,7 +2,9 @@ package com.terminalvelocitycabbage.exampleclient;
 
 import com.terminalvelocitycabbage.engine.client.renderer.Renderer;
 import com.terminalvelocitycabbage.engine.client.renderer.components.Camera;
+import com.terminalvelocitycabbage.engine.client.renderer.model.ColoredVertex;
 import com.terminalvelocitycabbage.engine.client.renderer.model.TexturedVertex;
+import com.terminalvelocitycabbage.engine.client.renderer.shapes.ColoredCuboid;
 import com.terminalvelocitycabbage.engine.client.renderer.shapes.TexturedRectangle;
 import com.terminalvelocitycabbage.engine.client.resources.Identifier;
 import com.terminalvelocitycabbage.engine.client.shader.ShaderProgram;
@@ -38,8 +40,8 @@ public class GameClientRenderer extends Renderer {
 				ASSETS_ROOT_RESOURCE_MANAGER,
 				new Identifier(GameClient.ID, "textures/kyle.png")
 		));
-		gameObjects.add(GameObject.builder().setModel(rectangleModel).build());
-		/*
+		//gameObjects.add(GameObject.builder().setModel(rectangleModel).build());
+
 		ColoredCuboidModel cuboidModel = new ColoredCuboidModel(new ColoredCuboid(
 				new ColoredVertex().setXYZ(-0.5f, 0.5f, 0f).setRGB(255, 0, 0),
 				new ColoredVertex().setXYZ(-0.5f, -0.5f, 0f).setRGB(0, 255, 0),
@@ -52,36 +54,23 @@ public class GameClientRenderer extends Renderer {
 		));
 		gameObjects.add(GameObject.builder().setModel(cuboidModel, false).build());
 
-		 */
-
 		//bind all Game Objects
 		for (GameObject gameObject : gameObjects) {
 			gameObject.bind();
 		}
 
 		//Create Shaders
+		//Create default shader that is used for textured elements
 		ShaderProgram defaultShaderHandler = new ShaderProgram();
-		defaultShaderHandler.createShader("defaultVertex", GL_VERTEX_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/default.vert"));
-		defaultShaderHandler.createShader("texturedFragment", GL_FRAGMENT_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/textured.frag"));
-		//TODO defaultShaderHandler.createShader("coloredFragment", GL_FRAGMENT_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/colored.frag"));
-		defaultShaderHandler.enableShader("defaultVertex");
-		defaultShaderHandler.enableShader("texturedFragment");
-		defaultShaderHandler.link();
+		defaultShaderHandler.queueShader(GL_VERTEX_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/textured.vert"));
+		defaultShaderHandler.queueShader(GL_FRAGMENT_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/textured.frag"));
+		defaultShaderHandler.bindAll();
 
-		//Init shader
-		defaultShaderHandler.use();
-
-		//Create uniform for projectionMatrix
-		defaultShaderHandler.createUniform("projectionMatrix");
-		//This will in the future have to be changed on command with in game fox sliders and things like that
-		//but for now since we dont have the implemented we can leave it like this
-		defaultShaderHandler.setUniformMat4f("projectionMatrix", camera.getProjectionMatrix());
-
-		//Create uniform for modelViewMatrix
-		//The worldMatrix is what tells Opengl that something is rotated or scaled from it's local state
-		//This includes the camera positions (it is the world Matrix and the view Matrix combined
-		defaultShaderHandler.createUniform("modelViewMatrix");
-		//The uniform has to be updated every frame so it will be done later
+		//Create shader for elements without textures but containing color data in the vertex
+		ShaderProgram coloredShaderHandler = new ShaderProgram();
+		coloredShaderHandler.queueShader(GL_VERTEX_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/colored.vert"));
+		coloredShaderHandler.queueShader(GL_FRAGMENT_SHADER, ASSETS_ROOT_RESOURCE_MANAGER, new Identifier(GameClient.ID, "shaders/colored.frag"));
+		coloredShaderHandler.bindAll();
 
 		//Init viewMatrix var
 		Matrix4f viewMatrix;
@@ -123,26 +112,25 @@ public class GameClientRenderer extends Renderer {
 			viewMatrix = camera.getViewMatrix();
 
 			//Draw whatever changes were pushed
-			//render all textured Game Objects
-			//TODO defaultShaderHandler.enableShader("texturedFragment");
 			for (GameObject gameObject : gameObjects) {
-				//Update the worldMatrix for the object with the new translations
-				defaultShaderHandler.setUniformMat4f("modelViewMatrix", gameObject.getModelViewMatrix(viewMatrix));
 				//Render the current object
+				if (gameObject.isTextured()) {
+					coloredShaderHandler.disable();
+					defaultShaderHandler.enable();
+					defaultShaderHandler.createUniform("projectionMatrix");
+					defaultShaderHandler.createUniform("modelViewMatrix");
+					defaultShaderHandler.setUniformMat4f("modelViewMatrix", gameObject.getModelViewMatrix(viewMatrix));
+					defaultShaderHandler.setUniformMat4f("projectionMatrix", camera.getProjectionMatrix());
+				} else {
+					defaultShaderHandler.disable();
+					coloredShaderHandler.enable();
+					coloredShaderHandler.createUniform("projectionMatrix");
+					coloredShaderHandler.createUniform("modelViewMatrix");
+					coloredShaderHandler.setUniformMat4f("modelViewMatrix", gameObject.getModelViewMatrix(viewMatrix));
+					coloredShaderHandler.setUniformMat4f("projectionMatrix", camera.getProjectionMatrix());
+				}
 				gameObject.render();
 			}
-			/*
-			defaultShaderHandler.disableShader("texturedFragment");
-			defaultShaderHandler.enableShader("coloredFragment");
-			for (GameObject gameObject : gameObjects) {
-				//Update the worldMatrix for the object with the new translations
-				defaultShaderHandler.setUniformMat4f("modelViewMatrix", gameObject.getModelViewMatrix(viewMatrix));
-				//Render the current object
-				gameObject.render();
-			}
-
-			 */
-
 
 			//Send the frame
 			push();
