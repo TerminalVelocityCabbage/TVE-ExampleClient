425591686fde9d8a5384972790ed78022eb4e7b0
Brandon Davis <neusfear@gmail.com>
2020-10-14 16:51:29 -0500

    Fix uv coordinates

--- src/main/java/com/terminalvelocitycabbage/exampleclient/models/loader/DCModelInfo.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/models/loader/DCModelInfo.java
@@ -161,32 +161,32 @@ public class DCModelInfo {
 			pixelUvs[0][0] = textureOffset.x + (2 * dimensions.z) + dimensions.x;
 			pixelUvs[0][1] = textureOffset.y + dimensions.z;
 			pixelUvs[0][2] = textureOffset.x + (2 * dimensions.z) + (2 * dimensions.x);
-			pixelUvs[0][3] = textureOffset.y + dimensions.x + dimensions.y;
+			pixelUvs[0][3] = textureOffset.y + dimensions.z + dimensions.y;
 
-			pixelUvs[1][0] = textureOffset.x + dimensions.z;
+			pixelUvs[1][0] = textureOffset.x + dimensions.z + dimensions.x;
 			pixelUvs[1][1] = textureOffset.y + dimensions.z;
 			pixelUvs[1][2] = textureOffset.x + (2 * dimensions.z) + dimensions.x;
-			pixelUvs[1][3] = textureOffset.y + dimensions.x + dimensions.y;
+			pixelUvs[1][3] = textureOffset.y + dimensions.z + dimensions.y;
 
 			pixelUvs[2][0] = textureOffset.x + dimensions.z;
 			pixelUvs[2][1] = textureOffset.y + dimensions.z;
 			pixelUvs[2][2] = textureOffset.x + dimensions.z + dimensions.x;
-			pixelUvs[2][3] = textureOffset.y + dimensions.x + dimensions.y;
+			pixelUvs[2][3] = textureOffset.y + dimensions.z + dimensions.y;
 
-			pixelUvs[3][0] = textureOffset.x + dimensions.z;
+			pixelUvs[3][0] = textureOffset.x;
 			pixelUvs[3][1] = textureOffset.y + dimensions.z;
-			pixelUvs[3][2] = textureOffset.x + dimensions.z + dimensions.x;
-			pixelUvs[3][3] = textureOffset.y + dimensions.x + dimensions.y;
+			pixelUvs[3][2] = textureOffset.x + dimensions.z;
+			pixelUvs[3][3] = textureOffset.y + dimensions.z + dimensions.y;
 
 			pixelUvs[4][0] = textureOffset.x + dimensions.z;
 			pixelUvs[4][1] = textureOffset.y;
 			pixelUvs[4][2] = textureOffset.x + dimensions.z + dimensions.x;
-			pixelUvs[4][3] = textureOffset.y + dimensions.x;
+			pixelUvs[4][3] = textureOffset.y + dimensions.z;
 
-			pixelUvs[5][0] = textureOffset.x + (2 * dimensions.z) + dimensions.x;
+			pixelUvs[5][0] = textureOffset.x + dimensions.z + dimensions.x;
 			pixelUvs[5][1] = textureOffset.y;
 			pixelUvs[5][2] = textureOffset.x + (2 * dimensions.z) + dimensions.x;
-			pixelUvs[5][3] = textureOffset.y + dimensions.x;
+			pixelUvs[5][3] = textureOffset.y + dimensions.z;
 
 			//clamp the values from 0 to 1
 			for (int i = 0; i < pixelUvs.length; i++) {
