0191169d6d311352ffeec722a59b815a4b8dd4a6
Brandon Davis <neusfear@gmail.com>
2020-12-19 16:01:01 -0600

    Use a scene for the demo world instead of having it all in the renderer

---
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/ExampleScene.java
@@ -0,0 +1,102 @@
+package com.terminalvelocitycabbage.exampleclient;
+
+import com.terminalvelocitycabbage.engine.client.renderer.Scene;
+import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.entity.ModeledGameObject;
+import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.DirectionalLight;
+import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.PointLight;
+import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.SpotLight;
+import com.terminalvelocitycabbage.engine.client.renderer.lights.Attenuation;
+import com.terminalvelocitycabbage.engine.client.renderer.model.AnimatedModel;
+import com.terminalvelocitycabbage.engine.client.renderer.model.Material;
+import com.terminalvelocitycabbage.engine.client.renderer.model.Texture;
+import com.terminalvelocitycabbage.engine.client.renderer.model.loader.AnimatedModelLoader;
+import com.terminalvelocitycabbage.engine.client.resources.Identifier;
+import net.dumbcode.studio.animation.events.AnimationEventRegister;
+import net.dumbcode.studio.animation.instance.ModelAnimationHandler;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+
+import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.*;
+import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.TEXTURE;
+import static org.lwjgl.glfw.GLFW.glfwGetTime;
+
+public class ExampleScene extends Scene {
+
+	@Override
+	public void init() {
+		//Load trex model to a Model object from dcm file
+		AnimatedModel trexModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "trex.dcm"));
+		trexModel.addAnimation("roar", ANIMATION, new Identifier(GameClient.ID, "roar.dca"));
+		trexModel.setMaterial(Material.builder().texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "trex.png"))).build());
+		//Create a game object from the model loaded and add the game object to the list of active objects
+		objectHandler.add("trex", ModeledGameObject.builder().setModel(trexModel).build());
+		objectHandler.getObject("trex").move(-50F, 0F, -30F);
+		trexModel.startAnimation("roar", true);
+
+		//Load gerald model to a Model object from dcm file
+		AnimatedModel robotModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "Gerald.dcm"));
+		robotModel.addAnimation("wave", ANIMATION, new Identifier(GameClient.ID, "wave.dca"));
+		robotModel.setMaterial(Material.builder().texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "gerald_base.png"))).build());
+		//Create a game object from the model loaded and add the game object to the list of active objects
+		ModeledGameObject robot = objectHandler.add("robot", ModeledGameObject.builder().setModel(robotModel).build());
+		objectHandler.getObject("robot").move(0F, 0F, -30F);
+		robotModel.startAnimation("wave");
+
+		robotModel.handler.setSrc(robot);
+		AnimationEventRegister.registerEvent("foo", (data, src) -> System.out.println(data + ", " + src));
+
+		//Do it again so we have two objects with different models and different textures
+		AnimatedModel wormModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "Worm.dcm"));
+		wormModel.setMaterial(Material.builder()
+				.texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "worm.png")))
+				.build());
+		objectHandler.add("worm", ModeledGameObject.builder().setModel(wormModel).build());
+		objectHandler.getObject("worm").move(0, 0, 10);
+
+		//bind all Game Objects
+		for (ModeledGameObject gameObject : objectHandler.getAllOfType(ModeledGameObject.class)) {
+			gameObject.bind();
+		}
+
+
+		//Create some light
+		Attenuation plAttenuation = new Attenuation(0.0f, 0.0f, 1.0f);
+		objectHandler.add("blueLight", new PointLight(new Vector3f(0, 2, -0.5f), new Vector3f(0,0,1), 1.0f, plAttenuation));
+		objectHandler.add("whiteLight", new PointLight(new Vector3f(0, 4, -0.5f), new Vector3f(1,1,1), 1.0f, plAttenuation));
+		Attenuation slAttenuation = new Attenuation(0.0f, 0.0f, 0.02f);
+		objectHandler.add("redSpotLight", new SpotLight(new Vector3f(0, 2, 0), new Vector3f(1, 0, 0), 1.0f, slAttenuation, new Vector3f(0, 1, 0), 140));
+		objectHandler.add("sun", new DirectionalLight(new Vector3f(-0.68f, 0.55f, 0.42f), new Vector4f(1, 1, 0.5f, 1), 1.0f));
+	}
+
+	@Override
+	public void update(long deltaTime) {
+
+		//Move around the point lights
+		objectHandler.getObject("blueLight").move(0, (float)Math.sin(glfwGetTime())/10, 0);
+		objectHandler.getObject("whiteLight").move(0, (float)Math.cos(glfwGetTime())/8, 0);
+
+		//Animate the model
+		ModeledGameObject trex = objectHandler.getObject("trex");
+		ModelAnimationHandler trexHandler = ((AnimatedModel) trex.getModel()).handler;
+		//TODO animation smoothness (don't hard code in 20)
+		trexHandler.animate(deltaTime / 1e9f * 20);
+
+		//Tell the engine that the game object needs to be re-rendered
+		trex.queueUpdate();
+		ModeledGameObject robot = objectHandler.getObject("robot");
+		ModelAnimationHandler robotHandler = ((AnimatedModel) robot.getModel()).handler;
+		//TODO animation smoothness (don't hard code in 20)
+		robotHandler.animate(deltaTime / 1e9f * 20);
+
+		//Tell the engine that the game object needs to be re-rendered
+		robot.queueUpdate();
+	}
+
+	@Override
+	public void destroy() {
+		//Cleanup
+		for (ModeledGameObject gameObject : objectHandler.getAllOfType(ModeledGameObject.class)) {
+			gameObject.destroy();
+		}
+	}
+}
--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameClientRenderer.java
@@ -3,44 +3,34 @@ package com.terminalvelocitycabbage.exampleclient;
 import com.terminalvelocitycabbage.engine.client.renderer.Renderer;
 import com.terminalvelocitycabbage.engine.client.renderer.components.Camera;
 import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.entity.ModeledGameObject;
-import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.DirectionalLight;
 import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.PointLight;
 import com.terminalvelocitycabbage.engine.client.renderer.gameobjects.lights.SpotLight;
-import com.terminalvelocitycabbage.engine.client.renderer.lights.Attenuation;
-import com.terminalvelocitycabbage.engine.client.renderer.model.Material;
-import com.terminalvelocitycabbage.engine.client.renderer.model.Texture;
-import com.terminalvelocitycabbage.engine.client.renderer.model.loader.AnimatedModelLoader;
 import com.terminalvelocitycabbage.engine.client.renderer.shader.ShaderHandler;
 import com.terminalvelocitycabbage.engine.client.renderer.shader.ShaderProgram;
 import com.terminalvelocitycabbage.engine.client.renderer.shapes.Rectangle;
 import com.terminalvelocitycabbage.engine.client.renderer.ui.*;
-import com.terminalvelocitycabbage.engine.client.renderer.util.GameObjectHandler;
 import com.terminalvelocitycabbage.engine.client.resources.Identifier;
-import com.terminalvelocitycabbage.engine.client.renderer.model.AnimatedModel;
-import net.dumbcode.studio.animation.events.AnimationEventRegister;
-import net.dumbcode.studio.animation.info.AnimationInfo;
-import net.dumbcode.studio.animation.info.AnimationLoader;
-import net.dumbcode.studio.animation.instance.ModelAnimationHandler;
-import org.joml.*;
+import org.joml.Matrix3f;
+import org.joml.Matrix4f;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
 
-import java.io.IOException;
-import java.lang.Math;
 import java.util.List;
-import java.util.UUID;
 
 import static com.terminalvelocitycabbage.engine.client.renderer.shader.Shader.Type.FRAGMENT;
 import static com.terminalvelocitycabbage.engine.client.renderer.shader.Shader.Type.VERTEX;
 import static com.terminalvelocitycabbage.engine.client.renderer.ui.UIDimension.Unit.PERCENT;
 import static com.terminalvelocitycabbage.engine.client.renderer.ui.UIDimension.Unit.PIXELS;
-import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.*;
-import static org.lwjgl.glfw.GLFW.glfwGetTime;
+import static com.terminalvelocitycabbage.exampleclient.GameResourceHandler.SHADER;
 import static org.lwjgl.opengl.GL20.*;
 
 public class GameClientRenderer extends Renderer {
 
-	private GameObjectHandler gameObjectHandler = new GameObjectHandler();
 	private final ShaderHandler shaderHandler = new ShaderHandler();
 	private GameInputHandler inputHandler = new GameInputHandler();
+
+	//TODO create a scene handler
+	private ExampleScene exampleScene = new ExampleScene();
 
 	private UICanvas testCanvas = new UICanvas(getWindow());
 
@@ -72,40 +62,6 @@ public class GameClientRenderer extends Renderer {
 		testCanvas.addContainer(new UIContainer(new UIDimension(400, PIXELS), new UIDimension(40, PERCENT), new UIAnchor(AnchorPoint.BOTTOM_MIDDLE, AnchorDirection.UP), new UIStyle().setColor(1, 0, 1, 1).marginBottom(10, PIXELS)));
 		testCanvas.queueUpdate();
 
-		//Load trex model to a Model object from dcm file
-		AnimatedModel trexModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "trex.dcm"));
-		trexModel.addAnimation("roar", ANIMATION, new Identifier(GameClient.ID, "roar.dca"));
-		trexModel.setMaterial(Material.builder().texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "trex.png"))).build());
-		//Create a game object from the model loaded and add the game object to the list of active objects
-		gameObjectHandler.add("trex", ModeledGameObject.builder().setModel(trexModel).build());
-		gameObjectHandler.getObject("trex").move(-50F, 0F, -30F);
-		trexModel.startAnimation("roar", true);
-
-		//Load gerald model to a Model object from dcm file
-		AnimatedModel robotModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "Gerald.dcm"));
-		robotModel.addAnimation("wave", ANIMATION, new Identifier(GameClient.ID, "wave.dca"));
-		robotModel.setMaterial(Material.builder().texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "gerald_base.png"))).build());
-		//Create a game object from the model loaded and add the game object to the list of active objects
-		ModeledGameObject robot = gameObjectHandler.add("robot", ModeledGameObject.builder().setModel(robotModel).build());
-		gameObjectHandler.getObject("robot").move(0F, 0F, -30F);
-		robotModel.startAnimation("wave");
-
-		robotModel.handler.setSrc(robot);
-		AnimationEventRegister.registerEvent("foo", (data, src) -> System.out.println(data + ", " + src));
-
-		//Do it again so we have two objects with different models and different textures
-		AnimatedModel wormModel = AnimatedModelLoader.load(MODEL, new Identifier(GameClient.ID, "Worm.dcm"));
-		wormModel.setMaterial(Material.builder()
-				.texture(new Texture(TEXTURE, new Identifier(GameClient.ID, "worm.png")))
-				.build());
-		gameObjectHandler.add("worm", ModeledGameObject.builder().setModel(wormModel).build());
-		gameObjectHandler.getObject("worm").move(0, 0, 10);
-
-		//bind all Game Objects
-		for (ModeledGameObject gameObject : gameObjectHandler.getAllOfType(ModeledGameObject.class)) {
-			gameObject.bind();
-		}
-
 		//Create Shaders
 		//Create default shader that is used for textured elements
 		shaderHandler.newProgram("default");
@@ -128,13 +84,8 @@ public class GameClientRenderer extends Renderer {
 		//Store InputHandler
 		inputHandler = (GameInputHandler) getWindow().getInputHandler();
 
-		//Create some light
-		Attenuation plAttenuation = new Attenuation(0.0f, 0.0f, 1.0f);
-		gameObjectHandler.add("blueLight", new PointLight(new Vector3f(0, 2, -0.5f), new Vector3f(0,0,1), 1.0f, plAttenuation));
-		gameObjectHandler.add("whiteLight", new PointLight(new Vector3f(0, 4, -0.5f), new Vector3f(1,1,1), 1.0f, plAttenuation));
-		Attenuation slAttenuation = new Attenuation(0.0f, 0.0f, 0.02f);
-		gameObjectHandler.add("redSpotLight", new SpotLight(new Vector3f(0, 2, 0), new Vector3f(1, 0, 0), 1.0f, slAttenuation, new Vector3f(0, 1, 0), 140));
-		gameObjectHandler.add("sun", new DirectionalLight(new Vector3f(-0.68f, 0.55f, 0.42f), new Vector4f(1, 1, 0.5f, 1), 1.0f));
+		//Init the scene
+		exampleScene.init();
 
 		//For wireframe mode
 		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -156,24 +107,6 @@ public class GameClientRenderer extends Renderer {
 		//Update camera rotation
 		camera.rotate(inputHandler.getDisplayVector().mul(0.4f), inputHandler.getCameraRollVector() * 0.05f);
 
-		//Move around the point lights
-		gameObjectHandler.getObject("blueLight").move(0, (float)Math.sin(glfwGetTime())/10, 0);
-		gameObjectHandler.getObject("whiteLight").move(0, (float)Math.cos(glfwGetTime())/8, 0);
-
-		//Animate the model
-		ModeledGameObject trex = gameObjectHandler.getObject("trex");
-		ModelAnimationHandler trexHandler = ((AnimatedModel) trex.getModel()).handler;
-		trexHandler.animate(this.getAnimationDeltaTime());
-
-		//Tell the engine that the game object needs to be re-rendered
-		trex.queueUpdate();
-		ModeledGameObject robot = gameObjectHandler.getObject("robot");
-		ModelAnimationHandler robotHandler = ((AnimatedModel) robot.getModel()).handler;
-		robotHandler.animate(this.getAnimationDeltaTime());
-
-		//Tell the engine that the game object needs to be re-rendered
-		robot.queueUpdate();
-
 		//Update the view Matrix with the current camera position
 		//This has to happen before game items are updated
 		viewMatrix.identity().set(camera.getViewMatrix());
@@ -185,6 +118,9 @@ public class GameClientRenderer extends Renderer {
 		//Since the text rendering is so awful I'm just going to use the window title for now
 		getWindow().setTitle("FPS: " + String.valueOf(getFramerate()).split("\\.")[0] + " (" + getFrameTimeAverageMillis() + "ms)");
 
+		//Update the scene
+		exampleScene.update(getDeltaTime());
+
 		//Send the frame
 		push();
 	}
@@ -192,13 +128,10 @@ public class GameClientRenderer extends Renderer {
 	@Override
 	public void destroy() {
 		super.destroy();
-		//Cleanup
-		for (ModeledGameObject gameObject : gameObjectHandler.getAllOfType(ModeledGameObject.class)) {
-			gameObject.destroy();
-		}
 		shaderHandler.cleanup();
 		testCanvas.getContainers().forEach(UIRenderableElement::destroy);
 		testCanvas.destroy();
+		exampleScene.destroy();
 	}
 
 	private void renderNormalsDebug(Camera camera, Matrix4f viewMatrix, ShaderProgram shaderProgram) {
@@ -211,7 +144,7 @@ public class GameClientRenderer extends Renderer {
 		shaderProgram.createUniform("normalTransformationMatrix");
 
 		//Draw whatever changes were pushed
-		for (ModeledGameObject gameObject : gameObjectHandler.getAllOfType(ModeledGameObject.class)) {
+		for (ModeledGameObject gameObject : exampleScene.getObjectsOfType(ModeledGameObject.class)) {
 			gameObject.update();
 			shaderProgram.setUniform("projectionMatrix", camera.getProjectionMatrix());
 			shaderProgram.setUniform("modelViewMatrix", gameObject.getModelViewMatrix(viewMatrix));
@@ -225,9 +158,9 @@ public class GameClientRenderer extends Renderer {
 		shaderProgram.enable();
 
 		//Update positions of concerned lights in view space (point and spot lights)
-		List<PointLight> pointLights = gameObjectHandler.getAllOfType(PointLight.class);
+		List<PointLight> pointLights = exampleScene.getObjectsOfType(PointLight.class);
 		pointLights.forEach(light -> light.update(viewMatrix));
-		List<SpotLight> spotLights = gameObjectHandler.getAllOfType(SpotLight.class);
+		List<SpotLight> spotLights = exampleScene.getObjectsOfType(SpotLight.class);
 		spotLights.forEach(light -> light.update(viewMatrix));
 
 		//Render the current object
@@ -244,7 +177,7 @@ public class GameClientRenderer extends Renderer {
 		shaderProgram.createMaterialUniform("material");
 
 		//Draw whatever changes were pushed
-		for (ModeledGameObject gameObject : gameObjectHandler.getAllOfType(ModeledGameObject.class)) {
+		for (ModeledGameObject gameObject : exampleScene.getObjectsOfType(ModeledGameObject.class)) {
 
 			gameObject.update();
 
@@ -256,7 +189,7 @@ public class GameClientRenderer extends Renderer {
 			shaderProgram.setUniform("specularPower", 10.0f); //Reflected light intensity
 			pointLights.forEach(light -> shaderProgram.setUniform("pointLights", light, pointLights.indexOf(light)));
 			spotLights.forEach(light -> shaderProgram.setUniform("spotLights", light, spotLights.indexOf(light)));
-			shaderProgram.setUniform("directionalLight", gameObjectHandler.getObject("sun"));
+			shaderProgram.setUniform("directionalLight", exampleScene.objectHandler.getObject("sun"));
 			//Material stuff
 			shaderProgram.setUniform("material", gameObject.getModel().getMaterial());
 
