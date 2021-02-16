647024ff24db4dfea1be22acdf38c496cd605803
Brandon Davis <neusfear@gmail.com>
2021-02-16 14:51:54 -0600

    Png decoder 2 (#15)
    * Convert fonts
    
    * update engine

--- src/main/java/com/terminalvelocitycabbage/exampleclient/GameResourceHandler.java
+++ src/main/java/com/terminalvelocitycabbage/exampleclient/GameResourceHandler.java
@@ -3,10 +3,13 @@ package com.terminalvelocitycabbage.exampleclient;
 import com.terminalvelocitycabbage.engine.client.resources.ClassLoaderResourceManager;
 import com.terminalvelocitycabbage.engine.client.resources.ResourceManager;
 
+import java.io.File;
+
 public class GameResourceHandler {
 	public static final ResourceManager MODEL = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "models");
 	public static final ResourceManager ANIMATION = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "animations");
 	public static final ResourceManager TEXTURE = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "textures");
-	public static final ResourceManager FONT = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "fonts");
+	public static final ResourceManager FONT_INFO = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "fonts" + File.separator + "info");
+	public static final ResourceManager FONT_TEXTURE = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "fonts" + File.separator + "texture");
 	public static final ResourceManager SHADER = new ClassLoaderResourceManager(ClassLoader.getSystemClassLoader(), "assets", "shaders");
 }
